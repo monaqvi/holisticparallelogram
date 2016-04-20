0d35ac08955e08aeb9f2be09322b2e1f02167e8e
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-04-20 16:22:38 -0700

    Rename auth to specify Google

--- server/auth/authGoogle.js
+++ server/auth/authGoogle.js
@@ -17,7 +17,7 @@ module.exports.handleLogin = passport.authenticate('google', {
   scope: ['https://www.googleapis.com/auth/plus.login']
 });
 
-module.exports.authenticateGoogleLogin = passport.authenticate('google', {
+module.exports.authenticateLogin = passport.authenticate('google', {
   failureRedirect: '/'
 });
 
