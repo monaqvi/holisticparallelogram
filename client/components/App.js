8f3a10d928aa2a3254e7ed3f73303113bfc6b267
Biletnikoff <bogere.sanders@gmail.com>
2016-04-21 20:54:01 -0700

    add multiple tab functionality using material-ui

--- client/components/App.js
+++ client/components/App.js
@@ -1,25 +1,52 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import actions from '../actions/index.js';
-
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
 import Header from './Header';
 import PlaceContainer from './PlaceContainer';
 import SavedPlaceContainer from './SavedPlaceContainer';
 import searchGooglePlaces from '../utils/searchGooglePlaces.js';
+import touchTap from 'react-tap-event-plugin';
 import $ from 'jquery';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a'
+    };
+  }
+
+  handleChange(e, index, value){
+    console.log(arguments);
+console.log('fire');
+    this.setState({
+      value: e
+    });
+  };
+
   render() {
     return (
       <div>
         <Header/>
-        <div className='grid'>
-          <PlaceContainer/>
-          <SavedPlaceContainer/>
-        </div>
+        <Tabs
+          value={this.state.value}
+        >
+            <Tab label='View Search' value='a' onClick={this.handleChange.bind(this, 'a')} ref='TEST'>
+            <div className='grid'>
+              <PlaceContainer/>
+            </div>
+            </Tab>
+            <Tab label='Your Places' value='b' onClick={this.handleChange.bind(this, 'b')} ref='TEST'>
+            <SavedPlaceContainer/>
+            </Tab>
+        </Tabs>
       </div>
     );
   }
 }
+
+
 
 export default App;
