66cb84490e0771dd929c8b6395447bcddf8876ef
clardizabal <chris.lardizabal@gmail.com>
2016-04-22 19:13:05 -0700

    fix heart position

--- client/components/PlaceEntry.js
+++ client/components/PlaceEntry.js
@@ -15,6 +15,9 @@ class PlaceEntry extends Component {
   render() {
     return (
       <div className='place-entry animated fadeInUp'>
+        <div className='place-entry-favorite'>
+          <span onClick={this.handleClick.bind(this)} className='icon-heart' aria-hidden='true'></span>
+        </div>
         <div className='place-info' >
             <h4>{ this.props.place.name }</h4>
             <p>{ this.props.place.address }</p>
@@ -26,9 +29,6 @@ class PlaceEntry extends Component {
               <a className='place-entry-link' href={'//www.google.com/search?q=' + this.props.place.name + ' ' + this.props.place.address}
               target='_blank'>Find on Google</a>
             </div>
-        </div>
-        <div className='place-entry-favorite'>
-          <span onClick={this.handleClick.bind(this)} className='icon-heart' aria-hidden='true'></span>
         </div>
       </div>
     );
